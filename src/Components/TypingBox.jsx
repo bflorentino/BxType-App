5fab2d4b89a9aab8c0b017550eebe50098d732b0
Bryan <bryanmontero81@gmail.com>
2022-01-31 23:57:43 -0400

    Some improvements in Typing Test Logic

--- src/Components/TypingBox.jsx
+++ src/Components/TypingBox.jsx
@@ -9,11 +9,11 @@ const TypingBox = ( ) => {
   const { formValues, handleInputChanges, reset } = useForm({
     typedWord : ""
   }) 
-
   const countingPressed = useRef(0) // Reference to counting the pressed chars by every word typed
-  const currentStringTyped = useRef("");
+  const  lastPressedKeyCode = useRef(null) // Reference to last pressed key.
   const [ started, setStarted ] = useState(false);
   const { words, theme } = useContext(typingContext)
+  const [ keyCode, setKeyCode ] = useState(0);
 
   // Necessary counters for typing test 
   const { 
@@ -30,7 +30,7 @@ const TypingBox = ( ) => {
     if(started){
 
       const charPressed = formValues.typedWord[formValues.typedWord.length - 1]
-      
+
       // Check last pressed key is space. If true it means that a word was typed
       if (charPressed === " "){
   
@@ -50,22 +50,18 @@ const TypingBox = ( ) => {
         
         counterRowWords.increment()
         reset() // Reset form input value every time a word is typed
-        setCurrentWord(words[counterRowWords.counter + 1].word)
+        setCurrentWord(words[counterRowWords.counter + 1].word) // Set Next word in textBox
         countingPressed.current = -1;
       }
       else{ // Case it was pressed any key different to space bar. It means a word is being typed
         
-        // Check if the string in the input is the same as the last string. If it's equal it means the backspace key was pressed
-        // In this case, the chars count of the current typed string will decrease, in opposite case it means it was pressed any 
-        //other key different to backspace, so the chars count of current typed string will increase.
-        (formValues.typedWord.trim().length < currentStringTyped.current && countingPressed.current > -1)
-        ?countingPressed.current--
-        :countingPressed.current++
+        // Check if key pressed was Backspace. In case true, It will increase current string chars count, in opposite case it will decrease.
+        ( keyCode === 8 && countingPressed.current > -1 )
+          ?countingPressed.current--
+          :countingPressed.current++
         
-        currentStringTyped.current = formValues.typedWord.trim().length; //This set the current input value as the current string typed
-        
+        // Comparing string typed with a substring from 0 to the couting of Pressed Keys in current word. If it's different it means word is incorrectly typed
         if(formValues.typedWord.trim() !== currentWord.substring(0,countingPressed.current)){
-          // In case the current string typed doesn't match with the current word to type it means the word is being typed incorrectly
           document.getElementById(words[counterRowWords.counter].id).classList.remove("dark:text-white")
           document.getElementById(words[counterRowWords.counter].id).classList.add("incorrect")
           counterIncorrectChars.increment();
@@ -74,8 +70,8 @@ const TypingBox = ( ) => {
           document.getElementById(words[counterRowWords.counter].id).classList.remove("incorrect")
           if(theme === 'dark'){
             document.getElementById(words[counterRowWords.counter].id).classList.add("dark:text-white")
-          } 
-          counterCorrectChars.increment();
+          }
+          !(keyCode === 8 && lastPressedKeyCode.current !== 8) && counterCorrectChars.increment();          
         }
       }
     }
@@ -84,6 +80,8 @@ const TypingBox = ( ) => {
 
   const handleKeyPressed = ( e ) => {
     !started && setStarted(true)
+    lastPressedKeyCode.current = keyCode;
+    setKeyCode(e.keyCode)
   }
  
   return (
@@ -95,7 +93,7 @@ const TypingBox = ( ) => {
         autoComplete='off'
         value={ formValues.typedWord }
         onChange={ handleInputChanges }
-        onKeyPress={ handleKeyPressed }
+        onKeyDown = { handleKeyPressed }
         className='w-3/4 h-3/4 py-2 outline-none text-2xl font-lato ml-4 px-2 rounded'
       />
       <Timer started={started}/>
